a68cc411bf592961f12c0a8c738d41cb8c0981be
Pedro84skynet <55103952+Pedro84skynet@users.noreply.github.com>
2020-10-02 17:50:07 -0300

    Corrigido: scriptPequeno estava como o médio

--- scriptPequeno.sh
+++ scriptPequeno.sh
@@ -6,13 +6,13 @@ make
 
 for i in {1..30}
 do
-   ./ep1 1 traceMedio.txt report3.txt $i 2> stderr.txt
+   ./ep1 1 tracePequeno.txt report3.txt $i 2> stderr.txt
 done
 for i in {1..30}
 do
-   ./ep1 2 traceMedio.txt report3.txt $i 2> stderr.txt
+   ./ep1 2 tracePequeno.txt report3.txt $i 2> stderr.txt
 done
 for i in {1..30}
 do
-   ./ep1 3 traceMedio.txt report3.txt $i 2> stderr.txt
+   ./ep1 3 tracePequeno.txt report3.txt $i 2> stderr.txt
 done
